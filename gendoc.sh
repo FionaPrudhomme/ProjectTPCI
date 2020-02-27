9b57732369ba0a816f7e90d09bbe573856dac0cc
FionaPrudhomme <38217841+FionaPrudhomme@users.noreply.github.com>
2020-02-27 13:29:31 +0100

    Update gendoc.sh

--- gendoc.sh
+++ gendoc.sh
@@ -9,7 +9,7 @@ rm -rf "$DOCS_DIR"
 mkdir -p "$DOCS_DIR"
 
 # "Build" the documentation
-cp target/site/index/html "$DOCS_DIR/index.html"
+cp target/site/index.html "$DOCS_DIR/index.html"
 
 echo "Documentation successfully built"
 
